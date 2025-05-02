9b47eb1d1a1ff153b88d918f9b1a1a6d3082f003
Skuddin <sheikh@nextgenitltd.com>
2025-05-02 07:56:21 +0600

    seeder

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Offer extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'price',
@@ -13,4 +17,14 @@ class Offer extends Model
         'author_id',
         'status'
     ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function locations(): BelongsToMany
+    {
+        return $this->belongsToMany(Location::class);
+    }
 }
